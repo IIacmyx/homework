e9e00bbc88e3e085496a236ba42a011484794902
IIacmyx <iiacmyxxxx@mail.ru>
2026-04-03 01:15:57 +0300

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,14 @@
+const firstName = 'Nikita';
+const lastName = 'Mashinin';
+const isStudent = true; 
+const age = 23; 
+const currentYear = 2025;
+const birthYear = currentYear - age;
+console.log(`Меня зовут ${firstName} ${lastName}, мне ${age} лет. Я ученик/ученица курса: ${isStudent}.`);
+let a = '123';
+let b = +'456';
+let c = Number('789');
+let d = Boolean(0);
+let e = Boolean(' ');
+let result = a + b + c + d + e;
+console.log(result);
